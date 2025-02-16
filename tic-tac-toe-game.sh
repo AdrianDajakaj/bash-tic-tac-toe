1febb5f7e880012b73337f72fa93cb016815941f
AdrianDajakaj <adrian.dajakaj.99@gmail.com>
2025-02-16 19:26:14 +0100

    final

--- tic-tac-toe-game.sh
+++ tic-tac-toe-game.sh
@@ -1,13 +1,15 @@
-declare -A game_board row_count col_count diag_count anti_diag_count
+declare -A game_board
 declare -i board_size=3
 
 possible_moves=()
 players_symbols=()
 selected_save=""
+game_mode=""
 current_player=""
 current_turn=""
 winner=""
 is_win=false
+game_status=""
 
 game_board_init(){
     is_win=false
@@ -69,8 +71,11 @@ game_save() {
     local save_file=$(date +"%Y-%m-%d_%H-%M-%S").txt
 
     echo "Saving game to $save_file"
+    echo "Game Mode: $game_mode" >> "$save_file" 
     echo "Current Player: $current_player" >> "$save_file"
     echo "Current Turn: $current_turn" >> "$save_file"
+    echo "Player 1 Symbol: ${players_symbols[0]}" >> "$save_file"
+    echo "Player 2 Symbol: ${players_symbols[1]}" >> "$save_file"
     echo "Current Board:">> "$save_file"
 
     for i in {0..2}; do
@@ -92,19 +97,46 @@ game_save_chooser(){
     done
     local saves_count=${#saves_list[@]}
     while true; do
-        echo "Select game save number (1 to $saves_count):"
+        echo "Select game save number (1 to $saves_count) or 'M' to return to the main menu:"
         read user_input
         if [[ "$user_input" =~ ^[0-9]+$ ]]; then
             if (( user_input >= 1 && user_input <= saves_count )); then
                 selected_save="${saves_list[$((user_input-1))]}"
                 formatted_save_name=$(echo "$selected_save" | sed 's/_/ /g; s/-/:/g; s/.txt//')
                 echo "Selected save: $formatted_save_name"
-                break  
+                
+                echo "What would you like to do?"
+                echo "1. Load the game"
+                echo "2. Remove the game save"
+                echo "3. Return"
+                read action_choice
+                
+                case $action_choice in
+                    1)
+                        game_save_load "$selected_save"
+                        break
+                        ;;
+                    2)
+                        game_save_remove "$selected_save"
+                        game_save_chooser
+                        ;;
+                    3)
+                        game_save_chooser
+                        ;;
+                    *)
+                        echo "Invalid choice. Try again."
+                        game_save_chooser
+                        ;;
+                esac
             else
                 echo "The number must be between 1 and $saves_count! Try again."
             fi
+        elif [[ "$user_input" == "M" || "$user_input" == "m" ]]; then
+            echo "Returning to the main menu..."
+            main_menu
+            break
         else
-            echo "It's not a number! Try again."
+            echo "Invalid input. Try again."
         fi
     done
 }
@@ -113,15 +145,21 @@ game_save_load() {
     local save_file=$1
     if [[ ! -f "$save_file" ]]; then
         echo "Save game does not exist: $save_file"
-        return 1
+        main_menu
     fi
     echo "Loading game..."
 
     while IFS= read -r line; do
-        if [[ "$line" =~ ^Current\ Player:\ (.+)$ ]]; then
+        if [[ "$line" =~ ^Game\ Mode:\ (.+)$ ]]; then
+            game_mode="${BASH_REMATCH[1]}"
+        elif [[ "$line" =~ ^Current\ Player:\ (.+)$ ]]; then
             current_player="${BASH_REMATCH[1]}"
         elif [[ "$line" =~ ^Current\ Turn:\ (.+)$ ]]; then
             current_turn="${BASH_REMATCH[1]}"
+        elif [[ "$line" =~ ^Player\ 1\ Symbol:\ (.+)$ ]]; then
+            players_symbols[0]="${BASH_REMATCH[1]}"
+        elif [[ "$line" =~ ^Player\ 2\ Symbol:\ (.+)$ ]]; then
+            players_symbols[1]="${BASH_REMATCH[1]}"
         elif [[ "$line" == "Current Board:" ]]; then
             board_lines=()
         elif [[ -n "${board_lines[@]}" ]]; then
@@ -137,7 +175,13 @@ game_save_load() {
         done
         ((i++))
     done
-    echo "Game loaded successfully!"
+    if [[ "$game_mode" == "multiplayer" ]]; then
+        echo "Multiplayer game loaded successfully!"
+        game_status="running"
+        multiplayer_game
+    else
+        echo "Singleplayer game loaded successfully!"
+    fi
 }
 
 game_save_remove(){
@@ -198,88 +242,208 @@ get_player_by_symbol(){
 }
 
 win_checker() {
-    local row=$1
-    local col=$2
-    local player_symbol=$3
-    (( row_count["$player_symbol,$row"]++ ))
-    (( col_count["$player_symbol,$col"]++ ))
-    if (( row == col )); then
-        (( diag_count["$player_symbol"]++ ))
-    fi
-    if (( row + col == board_size - 1 )); then
-        (( anti_diag_count["$player_symbol"]++ ))
-    fi
-     echo ${diag_count["$player_symbol,$row"]}
-    if (( row_count["$player_symbol,$row"] == board_size || col_count["$player_symbol,$col"] == board_size || diag_count["$player_symbol"] == board_size || anti_diag_count["$player_symbol"] == board_size )); then
+    local player_symbol=$1
+    for row in 0 1 2; do
+        if [[ ${game_board["$row,0"]} == "$player_symbol" && ${game_board["$row,1"]} == "$player_symbol" && ${game_board["$row,2"]} == "$player_symbol" ]]; then
+            is_win=true
+            return
+        fi
+    done
+    for col in 0 1 2; do
+        if [[ ${game_board["0,$col"]} == "$player_symbol" && ${game_board["1,$col"]} == "$player_symbol" && ${game_board["2,$col"]} == "$player_symbol" ]]; then
+            is_win=true
+            return
+        fi
+    done
+    if [[ ${game_board["0,0"]} == "$player_symbol" && ${game_board["1,1"]} == "$player_symbol" && ${game_board["2,2"]} == "$player_symbol" ]]; then
         is_win=true
-    else
-        is_win=false
+        return
     fi
+    if [[ ${game_board["0,2"]} == "$player_symbol" && ${game_board["1,1"]} == "$player_symbol" && ${game_board["2,0"]} == "$player_symbol" ]]; then
+        is_win=true
+        return
+    fi
+    is_win=false
 }
-
 
 player_move() {
     local player=$1
     local move
     local row col
     while true; do
+        echo "=== Turn: $current_turn ==="
+        echo "Current Player: $current_player (Symbol: ${players_symbols[$((current_player-1))]})"
+        game_board_print
         echo -n "Possible moves: "
         echo "${possible_moves[@]}"
-        echo "Player $player, enter your move:"
+        echo "Player $player, enter your move (or type 'S' to save the game, 'M' to return to main menu):"
         read move
+
+        if [[ "$move" == "S" ]]; then
+            game_save
+            echo "Game saved successfully. Continuing..."
+            continue
+        fi
+
+        if [[ "$move" == "M" ]]; then
+            game_status="menu"
+            break
+        fi
+
         if [[ "$move" =~ ^[A-Ca-c][1-3]$ ]]; then
             col=$(echo "$move" | cut -c1 | tr 'A-C' '0-2')
             row=$(echo "$move" | cut -c2-2 | tr '1-3' '0-2')
             if [[ ${game_board[$row,$col]} == "." ]]; then
                 game_board[$row,$col]=${players_symbols[$((player-1))]}
                 remove_used_move "$move"
-                win_checker "$row" "$col" "${players_symbols[$((player-1))]}"
-                game_board_print
+                win_checker "${players_symbols[$((player-1))]}"
+
                 if [[ $is_win == true ]]; then
+                    game_board_print
                     echo "Player $player (${players_symbols[$((player-1))]}) wins!"
-                    # exit 0
+                    game_status="win"
+                    break
                 fi
                 if [[ $is_win == false && ${#possible_moves[@]} -eq 0 ]]; then
-                    echo Draw! Nobody wins!
-                    # exit 0 
+                    game_board_print
+                    echo "Draw! Nobody wins!"
+                    game_status="draw"
+                    break
                 fi
                 break
             else
                 echo "This position is already taken. Try again."
             fi
         else
-            echo "Invalid input. Please enter a valid move from list."
+            echo "Invalid input."
         fi
     done
 }
 
+evaluate_board() {
+    win_checker "${players_symbols[1]}"
+    if [[ $is_win == true ]]; then
+        echo "computer"
+    fi
+    win_checker "${players_symbols[0]}"
+    if [[ $is_win == true ]]; then
+        echo "player"
+    fi
+    if [[ $is_win == false ]]; then
+        if [[ ${#possible_moves[@]} -eq 0 ]]; then
+            echo "draw"
+        else
+            echo "none"
+        fi
+    fi
+}
+
+computer_move() {
+    local move
+    local row col
+    echo "=== Turn: $current_turn ==="
+    echo "Current Player: Computer (Symbol: ${players_symbols[$((current_player-1))]})"
+    possible_moves_copy=("${possible_moves[@]}") 
+    random_index=$((RANDOM % ${#possible_moves_copy[@]}))
+    move="${possible_moves_copy[$random_index]}"
+
+    col=$(echo "$move" | cut -c1 | tr 'A-C' '0-2')
+    row=$(echo "$move" | cut -c2-2 | tr '1-3' '0-2')
+    echo "Computer moves to $move..."
+    game_board[$row,$col]=${players_symbols[1]}
+    remove_used_move "$move"
+    win_checker "${players_symbols[1]}"
+
+    if [[ $is_win == true ]]; then
+        game_board_print
+        echo "Computer wins!"
+        game_status="win"
+    fi
+    if [[ $is_win == false && ${#possible_moves[@]} -eq 0 ]]; then
+        game_board_print
+        echo "Draw! Nobody wins!"
+        game_status="draw"
+    fi
+}
+
+main_menu(){
+    echo "Main menu"
+    echo "1. Single Player"
+    echo "2. Multi Player"
+    echo "3. Load Game"
+    echo "4. Exit"
+
+    read -p "Choose an option (1-4): " choice
+    case $choice in
+        1) 
+            game_board_init
+            symbol_chooser
+            current_turn=1
+            current_player=1
+            game_status="running"
+            singleplayer_game 
+            ;;
+        2) 
+            game_board_init
+            symbol_chooser
+            current_turn=1
+            current_player=1
+            game_status="running"
+            multiplayer_game    
+            ;;
+        3) game_save_chooser ;;
+        4) exit 0 ;;
+        *) echo "Invalid option. Please try again." ;;
+    esac
+}
+
+singleplayer_game() {
+    game_mode="singleplayer"
+    while [[ $game_status == "running" ]]; do
+        if [[ $current_player -eq 1 ]]; then
+            player_move "$current_player"
+        else
+            computer_move
+        fi
+        if [[ $game_status != "running" ]]; then
+            break
+        fi
+        ((current_turn++))
+        if [[ $current_player -eq 1 ]]; then
+            current_player=2
+        else
+            current_player=1
+        fi
+    done
+    echo "Returning to the main menu..."
+    main_menu
+}
+
+
+multiplayer_game() {
+    game_mode="multiplayer"
+    while [[ $game_status == "running" ]]; do
+        player_move "$current_player"
+        if [[ $game_status != "running" ]]; then
+            break
+        fi
+        ((current_turn++))
+        if [[ $current_player -eq 1 ]]; then
+            current_player=2
+        else
+            current_player=1
+        fi
+    done
+    echo "Returning to the main menu..."
+    main_menu
+}
+
+main_menu
 
 
 
 
-game_board_init
 
-symbol_chooser
 
-player_move "1"
 
-player_move "2"
-
-player_move "1"
-
-player_move "2"
-
-player_move "1"
-
-player_move "2"
-player_move "1"
-player_move "2"
-player_move "1"
-player_move "2"
-player_move "1"
-
-# symbol_chooser
-# game_board_checker
-# get_player_by_symbol "X"
-# echo $current_player
 
